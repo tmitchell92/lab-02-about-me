4fd4d80bb6492c0cb55ff92bef4e6c5a0cf200da
Timothy Mitchell <timothy.p.mitchell92@gmail.com>
2017-04-04 13:31:59 -0700

    five questions about me, done

--- app.js
+++ app.js
@@ -1,11 +1,52 @@
 'use strict';
 
 console.log('Here we go');
-var pizza = prompt('Do you like pineapple on pizza?');
+var pizza = prompt('Do I like pineapple on pizza?');
 if (pizza.trim().toLowerCase() == 'yes' || pizza.trim().toLowerCase() == 'y')
 {
-  alert('Me too!');
+  alert('Good job!');
 }
 else {
-  alert('How do you live with yourself?');
+  alert('BZZZT');
 }
+console.log('Do I like pineapple on pizza? '  + pizza)
+
+var drink = prompt('Do I like to drink Long Island Ice Teas?');
+if (drink.trim().toLowerCase() == 'yes' || drink.trim().toLowerCase() == 'y')
+{
+  alert('Correct.');
+}
+else {
+  alert('Try again.');
+}
+console.log('Do I like to drink Long Island Ice Teas? '  + drink)
+
+var code = prompt('Do I enjoy coding?');
+if (code.trim().toLowerCase() == 'yes' || code.trim().toLowerCase() == 'y')
+{
+  alert('Yes, so far I do.');
+}
+else {
+  alert("I'm sorry, that is incorrect.");
+}
+console.log('Do I enjoy coding? '  + code)
+
+var height = prompt('Am I over 6 feet tall?');
+if (height.trim().toLowerCase() == 'yes' || height.trim().toLowerCase() == 'y')
+{
+  alert('A couple of couple of inches too short.');
+}
+else {
+  alert("Good guess.");
+}
+console.log('Am I over 6 feet tall? '  + height)
+
+var hapa = prompt('Am I hapa?');
+if (hapa.trim().toLowerCase() == 'yes' || hapa.trim().toLowerCase() == 'y')
+{
+  alert('Yup, half Korean, half white.');
+}
+else {
+  alert("Nah, I'm hapa.");
+}
+console.log('Am I hapa? '  + hapa)
