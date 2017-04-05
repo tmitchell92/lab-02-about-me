7cf64afd5194a99405de1e8e4e224ff4eed6c7a8
Timothy Mitchell <timothy.p.mitchell92@gmail.com>
2017-04-05 14:26:38 -0700

    finished six question, on to 7 question

--- app.js
+++ app.js
@@ -1,52 +1,84 @@
 'use strict';
-
-console.log('Here we go');
-var pizza = prompt('Do I like pineapple on pizza?');
-if (pizza.trim().toLowerCase() == 'yes' || pizza.trim().toLowerCase() == 'y')
+//
+// console.log('Here we go');
+// var pizza = prompt('Do I like pineapple on pizza?');
+// if (pizza.trim().toLowerCase() == 'yes' || pizza.trim().toLowerCase() == 'y')
+// {
+//   alert('Good job!');
+// }
+// else {
+//   alert('BZZZT');
+// }
+// console.log('Do I like pineapple on pizza? '  + pizza)
+//
+// var drink = prompt('Do I like to drink Long Island Ice Teas?');
+// if (drink.trim().toLowerCase() == 'yes' || drink.trim().toLowerCase() == 'y')
+// {
+//   alert('Correct.');
+// }
+// else {
+//   alert('Try again.');
+// }
+// console.log('Do I like to drink Long Island Ice Teas? '  + drink)
+//
+// var code = prompt('Do I enjoy coding?');
+// if (code.trim().toLowerCase() == 'yes' || code.trim().toLowerCase() == 'y')
+// {
+//   alert('Yes, so far I do.');
+// }
+// else {
+//   alert("I'm sorry, that is incorrect.");
+// }
+// console.log('Do I enjoy coding? '  + code)
+//
+// var height = prompt('Am I over 6 feet tall?');
+// if (height.trim().toLowerCase() == 'yes' || height.trim().toLowerCase() == 'y')
+// {
+//   alert('A couple of couple of inches too short.');
+// }
+// else {
+//   alert("Good guess.");
+// }
+// console.log('Am I over 6 feet tall? '  + height)
+//
+// var contacts = prompt('Do I wear contacts');
+// if (hapa.trim().toLowerCase() == 'yes' || hapa.trim().toLowerCase() == 'y')
+// {
+//   alert('Yes, since the age of 11.');
+// }
+// else {
+//   alert('Saving up for that Lasik.');
+// }
+// console.log('Do I wear contacts? '  + contacts);
+var fav_num = 7;
+for(var i = 4; i > 0 || fav_num; i--)
 {
-  alert('Good job!');
+  var guess = prompt('What is my favorite number?')
+  guess = parseInt(guess)
+  if (guess === fav_num){
+    alert('Correct!')
+    fav_num = false
+  }
+  else if (guess < fav_num){
+    alert('too low')
+  }
+  else if (guess > fav_num){
+    alert('too high')
+  }
+  else if(i===1)
+  {
+    alert('You are out of guesses')
+  }
 }
-else {
-  alert('BZZZT');
 }
-console.log('Do I like pineapple on pizza? '  + pizza)
-
-var drink = prompt('Do I like to drink Long Island Ice Teas?');
-if (drink.trim().toLowerCase() == 'yes' || drink.trim().toLowerCase() == 'y')
+var fav_Poke = ['gengar','bulbasuar','charizard','scyther']
+for(var i=6; i > 0 || fav_Poke; i--)
 {
-  alert('Correct.');
+  var poke_guess= prompt('What is my favorite Pokemon?')
+  poke_guess.trim().toLowerCase()
+  if (poke_guess === fav_Poke){
+    alert('Congrats')
+    fav_color = false
+  }
+  else if( )
 }
-else {
-  alert('Try again.');
-}
-console.log('Do I like to drink Long Island Ice Teas? '  + drink)
-
-var code = prompt('Do I enjoy coding?');
-if (code.trim().toLowerCase() == 'yes' || code.trim().toLowerCase() == 'y')
-{
-  alert('Yes, so far I do.');
-}
-else {
-  alert("I'm sorry, that is incorrect.");
-}
-console.log('Do I enjoy coding? '  + code)
-
-var height = prompt('Am I over 6 feet tall?');
-if (height.trim().toLowerCase() == 'yes' || height.trim().toLowerCase() == 'y')
-{
-  alert('A couple of couple of inches too short.');
-}
-else {
-  alert("Good guess.");
-}
-console.log('Am I over 6 feet tall? '  + height)
-
-var hapa = prompt('Am I hapa?');
-if (hapa.trim().toLowerCase() == 'yes' || hapa.trim().toLowerCase() == 'y')
-{
-  alert('Yup, half Korean, half white.');
-}
-else {
-  alert("Nah, I'm hapa.");
-}
-console.log('Am I hapa? '  + hapa)
