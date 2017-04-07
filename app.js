11a7c33d856cd96f180f17c4a77dcb0369bb06a1
Timothy Mitchell <timothy.p.mitchell92@gmail.com>
2017-04-07 11:30:40 -0700

    added auto indent and changed app.js

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 'use strict';
 var name = prompt('What is your name?')
-var num_correct = 0
+var numCorrect = 0
 console.log('Here we go');
-function fun_pizza(){ var pizza = prompt('Do I like pineapple on pizza?');
+function funPizza(){ var pizza = prompt('Do I like pineapple on pizza?');
 if (pizza.trim().toLowerCase() == 'yes' || pizza.trim().toLowerCase() == 'y')
 {
   alert('Good job!');
-  num_correct= num_correct+ 1
+  numCorrect++
 }
 else {
   alert('BZZZT');
 }
 console.log('Do I like pineapple on pizza? '  + pizza)
 }
-fun_pizza();
-function fun_drink(){var drink = prompt('Do I like to drink Long Island Ice Teas?');
+funPizza();
+function funDrink(){var drink = prompt('Do I like to drink Long Island Ice Teas?');
 if (drink.trim().toLowerCase() == 'yes' || drink.trim().toLowerCase() == 'y')
 {
-  num_correct= num_correct+ 1
+  numCorrect++
   alert('Correct.');
 }
 else {
@@ -25,11 +25,11 @@ else {
 }
 console.log('Do I like to drink Long Island Ice Teas? '  + drink)
 }
-fun_drink();
-function fun_code(){var code = prompt('Do I enjoy coding?');
+funDrink();
+function funCode(){var code = prompt('Do I enjoy coding?');
 if (code.trim().toLowerCase() == 'yes' || code.trim().toLowerCase() == 'y')
 {
-  num_correct= num_correct+ 1
+  numCorrect++
   alert('Yes, so far I do.');
 }
 else {
@@ -37,23 +37,23 @@ else {
 }
 console.log('Do I enjoy coding? '  + code)
 }
-fun_code();
-function fun_height(){var height = prompt('Am I over 6 feet tall?');
+funCode();
+function funHeight(){var height = prompt('Am I over 6 feet tall?');
 if (height.trim().toLowerCase() == 'yes' || height.trim().toLowerCase() == 'y')
 {
   alert('A couple of couple of inches too short.');
 }
 else {
-  num_correct= num_correct+ 1
+  numCorrect++
   alert("Good guess.");
 }
 console.log('Am I over 6 feet tall? '  + height)
 }
-fun_height();
-function fun_contacts(){var contacts = prompt('Do I wear contacts');
+funHeight();
+function funContacts(){var contacts = prompt('Do I wear contacts');
 if (contacts.trim().toLowerCase() == 'yes' || contacts.trim().toLowerCase() == 'y')
 {
-  num_correct= num_correct+ 1
+  numCorrect++
   alert('Yes, since the age of 11.');
 }
 else {
@@ -61,57 +61,53 @@ else {
 }
 console.log('Do I wear contacts? '  + contacts);
 }
-fun_contacts();
-var fav_num = 7;
-var stop = true;
-function fun_num(){for(var i = 0; i < 4 && stop==true; i++)
-{
-  console.log(i);
-  var guess = prompt('What is my favorite number?');
-  guess = parseInt(guess);
-  if (guess === fav_num){
-    stop=!stop;
-    alert('Correct!');
-    num_correct= num_correct+ 1
-  }
-  else if (guess < fav_num){
-    alert('too low');
-  }
-  else if (guess > fav_num){
-    alert('too high');
-  }
-  else if(i===3)
-  {
-    alert('You are out of guesses');
-  }
-}
-}
-fun_num();
-var fav_Poke = ['gengar','bulbasuar','charizard','scyther'];
-var end = true;
-function fun_poke(){for(var i=6; i > 0 && end; i--)
-{
-  var poke_guess= prompt('What is my favorite Pokemon?');
-  poke_guess.trim().toLowerCase();
+funContacts();
+var favNum = 7;
+function funNum(){
+  for(var tries=4; tries > 0; tries--){
+    var numberGuess = prompt('What is my favorite number?');
+    numberGuess = parseInt(numberGuess);
+    if (numberGuess === favNum){
+      tries=0;
+      alert('Correct!');
+      numCorrect++}
+      else if (numberGuess < favNum){
+        alert('too low');}
+        else if (numberGuess > favNum){
+          alert('too high');}
+          else{
+            alert('That is not a number.');
+          }
+        }
+      }
+      funNum();
+      var favPoke = ['gengar','bulbasuar','charizard','scyther'];
+      var tries, pokeGuess, i;
+      var userWon = false;
+      function funPoke(){
+        for(tries=6; tries > 0 && !userWon; tries--){
+          pokeGuess= prompt('What is my favorite Pokemon?');
+          pokeGuess.trim().toLowerCase();
 
-  if (poke_guess === fav_Poke[0] || poke_guess === fav_Poke[1] ||poke_guess === fav_Poke[2] || poke_guess === fav_Poke[3]){
-    num_correct= num_correct+ 1;
-    alert('Congrats, ' + 'the correct answers are one of these: ' + fav_Poke);
-    end = false;
-  }
-  else if(i === 1){
-    alert('Sorry but you have used all of your guesses, the correct answers is one of the following: ' + fav_Poke);
-    end = false;
-  }
-}
-}
-fun_poke();
-console.log(num_correct);
-if (num_correct<7){
-    alert('You got ' + num_correct + ' out of 7 correct, ' + name + ', try again for a perfect score.');
-}
-if (num_correct ===7){
-  alert('You got ' + num_correct + ' out of 7 correct, ' + name + ', you got a perfect score!');
-}
-//
-// }
+          for (i=0; i < pokeGuess.length; i++){
+            if(pokeGuess === favPoke[i]){
+              alert('Congrats, thats right.');
+              userWon = true;
+            }
+          }
+          if (!userWon){
+            alert('Try again.');}
+          }
+        }
+        funPoke();
+        alert('The pokemon are ' + favPoke.join(', ') + '.');
+
+        console.log(numCorrect);
+        if (numCorrect<7){
+          alert('You got ' + numCorrect + ' out of 7 correct, ' + name + ', try again for a perfect score.');
+        }
+        if (numCorrect ===7){
+          alert('You got ' + numCorrect + ' out of 7 correct, ' + name + ', you got a perfect score!');
+        }
+
+      }
--- app2.js
+++ app2.js
@@ -65,50 +65,50 @@ funContacts();
 var favNum = 7;
 var stop = true;
 function funNum(){for(var i = 0; i < 4 && stop==true; i++)
-{
-  console.log(i);
-  var guess = prompt('What is my favorite number?');
-  guess = parseInt(guess);
-  if (guess === favNum){
-    stop=!stop;
-    alert('Correct!');
-    numCorrect++
-  }
-  else if (guess < favNum){
-    alert('too low');
-  }
-  else if (guess > favNum){
-    alert('too high');
-  }
-  else if(i===3)
   {
-    alert('You are out of guesses');
+    console.log(i);
+    var guess = prompt('What is my favorite number?');
+    guess = parseInt(guess);
+    if (guess === favNum){
+      stop=!stop;
+      alert('Correct!');
+      numCorrect++
+    }
+    else if (guess < favNum){
+      alert('too low');
+    }
+    else if (guess > favNum){
+      alert('too high');
+    }
+    else if(i===3)
+    {
+      alert('You are out of guesses');
+    }
   }
-}
 }
 funNum();
 var favPoke = ['gengar','bulbasuar','charizard','scyther'];
 var end = true;
 function funPoke(){for(var i=6; i > 0 && end; i--)
-{
-  var pokeGuess= prompt('What is my favorite Pokemon?');
-  pokeGuess.trim().toLowerCase();
+  {
+    var pokeGuess= prompt('What is my favorite Pokemon?');
+    pokeGuess.trim().toLowerCase();
 
-  if (pokeGuess === favPoke[0] || pokeGuess === favPoke[1] ||pokeGuess === favPoke[2] || pokeGuess === favPoke[3]){
-    numCorrect++;
-    alert('Congrats, ' + 'the correct answers are one of these: ' + favPoke);
-    end = false;
+    if (pokeGuess === favPoke[0] || pokeGuess === favPoke[1] ||pokeGuess === favPoke[2] || pokeGuess === favPoke[3]){
+      numCorrect++;
+      alert('Congrats, ' + 'the correct answers are one of these: ' + favPoke);
+      end = false;
+    }
+    else if(i === 1){
+      alert('Sorry but you have used all of your guesses, the correct answers is one of the following: ' + favPoke);
+      end = false;
+    }
   }
-  else if(i === 1){
-    alert('Sorry but you have used all of your guesses, the correct answers is one of the following: ' + favPoke);
-    end = false;
-  }
-}
 }
 funPoke();
 console.log(numCorrect);
 if (numCorrect<7){
-    alert('You got ' + numCorrect + ' out of 7 correct, ' + name + ', try again for a perfect score.');
+  alert('You got ' + numCorrect + ' out of 7 correct, ' + name + ', try again for a perfect score.');
 }
 if (numCorrect ===7){
   alert('You got ' + numCorrect + ' out of 7 correct, ' + name + ', you got a perfect score!');
